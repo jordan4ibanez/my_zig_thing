32673e4d5b6958b1199e9da4466df1fa69587fa1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-07 22:14:51 -0400

    create first zig class :)

--- hello_world.zig
+++ hello_world.zig
@@ -1,7 +1,37 @@
 const std = @import("std");
+const print = std.debug.print;
 
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
 
-    try stdout.print("hello, {s}!\n", .{"World"});
+pub const MyClass = struct {
+    my_value: i32,
+
+    pub fn new() MyClass {
+        return MyClass {
+            .my_value = 0,
+        };
+    }
+
+    pub fn add(self: *MyClass, value: i32) void {
+        self.my_value += value;
+    }
+
+    pub fn debug(self: MyClass) void {
+        print("my_value is: {d}", .{self.my_value});
+    }
+
+};
+
+
+
+pub fn main() !void {    
+    
+    var classy = MyClass.new();
+
+    classy.add(5);
+
+    classy.add(10);
+
+    classy.debug();
+
+
 }
