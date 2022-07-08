f3135055ac856c33f12c41c3b75f4204f716ad05
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-08 02:25:58 -0400

    create functioning arraylist with slice output

--- hello_ecs.zig
+++ hello_ecs.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const print = std.debug.print;
+const ArrayList = std.ArrayList;
+const my_allocator = std.testing.allocator;
 
 pub fn main() !void {
+    var my_array_list = ArrayList(u8).init( my_allocator);
 
+    try my_array_list.append(5);
+
+    var allocated_things = my_array_list.allocatedSlice();
+
+    print("{d}", .{allocated_things[0]});
 }
